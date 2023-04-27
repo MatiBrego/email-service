4dd25879b7be11704da18a531931c3567d6c5422
MatiBrego <matias.bregoli@ing.austral.edu.ar>
2023-04-26 22:42:18 -0300

    Updated EsLint

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,35 +1,34 @@
-import {Injectable} from "@nestjs/common";
-import {UserDto} from "./dto/user.dto";
-import {PrismaClient} from "@prisma/client";
+import { Injectable } from "@nestjs/common";
+import { UserDto } from "./dto/user.dto";
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class UserRepository{
+export class UserRepository {
+  constructor(private readonly db: PrismaClient) {}
 
-    constructor(private readonly db: PrismaClient) {}
+  async create(username: string, email: string, password: string ): Promise<UserDto> {
+    const user = await this.db.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: password,
+      },
+    });
+    return new UserDto(user);
+  }
 
-    async create(username: string, email: string, password: string): Promise<UserDto>{
-        const user = await this.db.user.create({
-            data: {
-                username: username,
-                email: email,
-                password: password
-            }
-        })
-        return new UserDto(user)
-    }
+  //TODO Join findBy functions into one
+  async findByEmail(email: string): Promise<UserDto | null> {
+    const user = await this.db.user.findUnique({
+      where: { email: email },
+    });
+    return user ? new UserDto(user) : null;
+  }
 
-    //TODO Join findBy functions into one
-    async findByEmail(email: string): Promise<UserDto | null>{
-        const user = await this.db.user.findUnique({
-            where: {email: email}
-        })
-        return user? new UserDto(user): null
-    }
-
-    async findByUsername(username: string): Promise<UserDto | null>{
-        const user = await this.db.user.findUnique({
-            where: {username: username}
-        })
-        return user? new UserDto(user): null
-    }
+  async findByUsername(username: string): Promise<UserDto | null> {
+    const user = await this.db.user.findUnique({
+      where: { username: username },
+    });
+    return user ? new UserDto(user) : null;
+  }
 }
